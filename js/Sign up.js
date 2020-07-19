ae845e4e09181f42435b70d37199ad9cb3841cb6
561 - محمد عمار بوزلوف <mbuzlouf@gmail.com>
2020-07-19 23:26:56 +0300

    الذي علم بالقلم

--- js/Sign up.js
+++ js/Sign up.js
@@ -19,11 +19,3 @@ window.addEventListener("load", () => {
   const preload = document.querySelector(".preload");
   preload.classList.add("preload-finish");
 });
-function check() {
-  let p1 = document.getElementById("pass").value;
-  let p2 = document.getElementById("pass2").value;
-  let user = document.getElementById("T-name").value;
-  if (p1 != p2 || (p1 == "" && p2 == "") || p1 == "" || p2 == "") {
-    alert("hi");
-  }
-}
