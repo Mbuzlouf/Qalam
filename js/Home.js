764be70f22488a5ff7189dc8c6096ffde9c54d4e
561 - محمد عمار بوزلوف <mbuzlouf@gmail.com>
2020-07-19 22:44:28 +0300

    Update Home.js

--- js/Home.js
+++ js/Home.js
@@ -46,7 +46,7 @@ hambar.addEventListener("click", () => {
 // passname
 
 // ---
-document.getElementById("name2").innerHTML = localStorage.getItem("textvalue");
+// document.getElementById("name2").innerHTML = localStorage.getItem("textvalue");
 window.addEventListener("load", () => {
   const preload = document.querySelector(".preload");
   preload.classList.add("preload-finish");
